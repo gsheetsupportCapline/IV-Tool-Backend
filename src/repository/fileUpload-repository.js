59d8de2745853cf3fe6761056376192c1c2542f5
spandey0806 <shubhampandey0806@gmail.com>
2024-09-06 02:07:03 +0530

    changes in fileUpload repository

--- src/repository/fileUpload-repository.js
+++ src/repository/fileUpload-repository.js
@@ -1,1 +1,23 @@
  
+
+const s3 = require('../utils/s3.util');
+
+const uploadFileToS3 = async (file) => {
+  const uploadParams = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: Date.now().toString() + '-' + file.originalname,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+    ACL: 'public-read',
+  };
+
+  try {
+    const result = await s3.upload(uploadParams).promise();
+    return result.Location;
+  } catch (error) {
+    console.error('Error uploading file to S3:', error);
+    throw error;
+  }
+};
+
+module.exports = { uploadFileToS3 };
