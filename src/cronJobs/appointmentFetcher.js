029c4932627128502ec5c382dbf44c6e8296d0a8
CaplineServices <vishusharma@caplineservices.com>
2026-01-05 23:04:55 +0530

    appointments are fetching in 1 hour now

--- src/cronJobs/appointmentFetcher.js
+++ src/cronJobs/appointmentFetcher.js
@@ -1,24 +1,24 @@
-const cron = require('node-cron');
-const appointmentService = require('../services/appointment-service');
+const cron = require("node-cron");
+const appointmentService = require("../services/appointment-service");
 
-// Schedule the cron job to run every 3 hours
+// Schedule the cron job to run every 1 hour
 
 const setupJob = () => {
-  cron.schedule('0 */3 * * *', async () => {
+  cron.schedule("0 */1 * * *", async () => {
     try {
-      console.log('Running fetchAndSaveData every 3 hours');
+      console.log("Running fetchAndSaveData every 1 hour");
       const response = await appointmentService.fetchDataAndStoreAppointments();
-      console.log('result', response);
+      console.log("result", response);
       if (!response) {
         console.error(
-          'No response received from fetchDataAndStoreAppointments'
+          "No response received from fetchDataAndStoreAppointments"
         );
       } else {
         //
       }
     } catch (error) {
       console.error(
-        'Error occured while running fetchDataAndStoreAppointments:',
+        "Error occured while running fetchDataAndStoreAppointments:",
         error
       );
     }
